c71e4b97b62968542b02bab50ce27d8e6cfc26f6
Boonkung <boonkung1123@gmail.com>
2022-08-06 13:34:12 +0700

    add bottomnavigationbar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,10 +10,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+      ),
+      drawer: Drawer(),
+      body: const Center(
         child: Text("Hello world"),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.content_paste), label: "List"),
+        BottomNavigationBarItem(icon: Icon(Icons.person_search), label: "Info")
+      ]),
     );
   }
 }
